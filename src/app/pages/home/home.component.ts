efbe6f788377370d466ff32d6459047e40ca5445
Bence_Hordos <hordos.bence@gmail.com>
2023-04-03 07:22:03 +0200

    Fixes , Filters , Product display box

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 cols= 3;
+category:string | undefined;
   constructor() { }
 
   ngOnInit(): void {
   }
   onColumnsCountChange(colsNumber:number): void{
 this.cols= colsNumber;
+  }
+  onShowCategory(newCategory:string):void {
+this.category = newCategory;
   }
 }
